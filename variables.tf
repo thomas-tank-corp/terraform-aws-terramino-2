f914fdff1668dff238c7f6776bb5ae8748afa1b9
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2024-11-13 13:03:09 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,3 @@
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "org_id" {
-  type = string
-}
-
-variable "project_id" {
-  type = string
-}
-
 variable "waypoint_application" {
   type = string
 }
